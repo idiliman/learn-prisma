3564b8da11c770aa3ee8050b74e7581a8c2847a2
idil <idil.iman1999@gmail.com>
2023-02-28 17:03:40 +0800

    Initial commit

---
+++ script.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: { name: 'idil' },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect;
+  });
